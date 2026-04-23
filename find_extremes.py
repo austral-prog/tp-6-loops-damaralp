5f63ea2e318c9dba35c55346b46bd03ee0852aa1
damaralp <damaralpaz@mail.austral.edu.ar>
2026-04-23 15:15:56 -0300

    Update find_extremes.py

--- find_extremes.py
+++ find_extremes.py
@@ -9,7 +9,11 @@ def find_min(numbers):
     Ejemplo: find_min([5, 5, 5]) -> 5
     Ejemplo: find_min([-3, -1, -7]) -> -7
     """
-    return "ANSWER HERE"  # Remove this line and implement
+    minimum = numbers[0]
+    for n in numbers:
+        if n < minimum:
+            minimum = n
+    return minimum
 
 
 def find_max(numbers):
@@ -21,7 +25,11 @@ def find_max(numbers):
     Ejemplo: find_max([5, 5, 5]) -> 5
     Ejemplo: find_max([-3, -1, -7]) -> -1
     """
-    return "ANSWER HERE"  # Remove this line and implement
+    maximum = numbers[0]
+    for n in numbers:
+        if n > maximum:
+            maximum = n
+    return maximum
 
 
 def count_negatives(numbers):
@@ -33,4 +41,8 @@ def count_negatives(numbers):
     Ejemplo: count_negatives([1, 2, 3]) -> 0
     Ejemplo: count_negatives([-1, -2, -3]) -> 3
     """
-    return "ANSWER HERE"  # Remove this line and implement
+    count = 0
+    for n in numbers:
+        if n < 0:
+            count += 1
+    return count
